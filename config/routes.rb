4c02163c631fd202507cff0f60c6b8cf78949dc3
Nick Bucciarelli <nick.bucciarelli@gmail.com>
2014-06-13 10:04:33 -0400

    Added haml as templating engine for angular.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/template' => 'welcome#template'
   resources :exercises
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
